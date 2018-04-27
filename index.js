9b5b61ccdb12d1102ce5ef3793635fc27106f311
Sarah Geyer <sjgeyer@comcast.net>
2018-04-27 13:41:00 -0700

    initial file setup

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+if (!process.env.NODE_ENV) {
+  throw new Error('Undefined NODE_ENV');
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  require('babel-register');
+}
+
+require('./src/main');
